e1adb54df04d05d25674588c1e8fb3054647bc0f
Nguyen Xuan Hao <nxhao235@gmail.com>
2018-05-18 09:22:51 +0700

    add DTO DAO

---
+++ KaraokeManager/DAO/MatHangDAO.cs
@@ -0,0 +1,39 @@
+﻿using KaraokeManager.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaraokeManager.DAO
+{
+    class MatHangDAO
+    {
+        private static MatHangDAO instance;
+        public static MatHangDAO Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new MatHangDAO();
+                return instance;
+            }
+        }
+        private MatHangDAO() { }
+
+        public List<MatHangDTO> getMatHangList()
+        {
+            List<MatHangDTO> matHangList = new List<MatHangDTO>();
+            string query = "EXECUTE uspGetMatHang";
+            DataTable data = DataProvider.Instance.ExecuteQuery(query);
+            
+            foreach (DataRow row in data.Rows)
+            {
+                MatHangDTO matHang = new MatHangDTO(row);
+                matHangList.Add(matHang);
+            }
+            return matHangList;
+        }
+    }
+}
---
+++ KaraokeManager/Form2.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using DevExpress.XtraTab;
+using KaraokeManager.DAO;
+using KaraokeManager.DTO;
+
+namespace KaraokeManager
+{
+    public partial class Form2 : DevExpress.XtraEditors.XtraForm
+    {
+        private List<MatHangDTO> matHangList;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            LoadMatHangList();
+        }
+
+        private void barButtonItem8_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            UserControlMain.UCSuDungDichVu ucSuDungDichVu = new UserControlMain.UCSuDungDichVu();
+            addTabControl(ucSuDungDichVu, "Sử dụng dịch vụ", "icons8-adjust-16");
+        }
+
+        private void xtraTabControlMain_ControlAdded(object sender, ControlEventArgs e)
+        {
+            xtraTabControlMain.SelectedTabPageIndex = xtraTabControlMain.TabPages.Count - 1;
+        }
+
+        private void addTabControl(UserControl userControl, string itemTabName, string icon)
+        {
+            bool isExist = false;
+
+            foreach(XtraTabPage tabItem in xtraTabControlMain.TabPages)
+            {
+                if (tabItem.Text == itemTabName)
+                {
+                    isExist = true;
+                    xtraTabControlMain.SelectedTabPage = tabItem;
+                }
+            }
+
+            if (isExist == false)
+            {
+                AddTab addTab = new AddTab();
+                addTab.AddTabControl(xtraTabControlMain, itemTabName, icon, userControl);
+            }
+        }
+
+        private void barButtonItem9_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            UserControlMain.UCThongKeDoanhThu ucThongKeDoanhThu = new UserControlMain.UCThongKeDoanhThu();
+            addTabControl(ucThongKeDoanhThu, "Thống kê doanh thu", "icons8-chart-16");
+        }
+
+        private void xtraTabControlMain_CloseButtonClick(object sender, EventArgs e)
+        {
+            xtraTabControlMain.TabPages.RemoveAt(xtraTabControlMain.SelectedTabPageIndex);
+            xtraTabControlMain.SelectedTabPageIndex = xtraTabControlMain.TabPages.Count - 1;
+        }
+
+        private void LoadMatHangList()
+        {
+            matHangList = MatHangDAO.Instance.getMatHangList();
+            dataGridView1.DataSource = matHangList;
+            gridControl1.DataSource = matHangList;
+        }
+    }
+}
